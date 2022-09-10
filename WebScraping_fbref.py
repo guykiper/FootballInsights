39e4b1bcf19855f680db2d53cbc14109d5048ecf
guykiper <guykiper1@gmail.com>
2022-09-10 20:55:53 +0300

    adding try-except in "create_url_season" adding else option in "get_link_report_match"

--- WebScraping_fbref.py
+++ WebScraping_fbref.py
@@ -5,22 +5,25 @@ import regex
 
 
 def create_url_season(year):
-    '''
+    """
     :param year: the year which the seasno start in
     :return: a dictionary , the key is the year and the value is the url site of the season
-    '''
+    """
     fbref_site = 'https://fbref.com/en/squads/53a2f082/'
     fbref_site_end = '/Real-Madrid-Stats'
     dict_season_url = {}
-    dict_season_url[str(year) + '-' + str(year + 1)] = (fbref_site + str(year) + '-' + str(year + 1) + fbref_site_end)
-    return (dict_season_url)
+    try:
+        dict_season_url[str(year) + '-' + str(year + 1)] = (fbref_site + str(year) + '-' + str(year + 1) + fbref_site_end)
+    except:
+        dict_season_url[str(year) + '-' + str(year + 1)] = "not found"
+    return dict_season_url
 
 
 def get_link_report_match(url):
-    '''
+    """
     :param url: url for the season
     :return: list with link to each game in the season
-    '''
+    """
     counter = 0
     list_links =[]
     page = requests.get(url)
@@ -29,13 +32,16 @@ def get_link_report_match(url):
     for link in links:
         counter += 1
         if ('href' in link.attrs) and ('matches' in str(link.attrs['href'])) and \
-                ('La-Liga' in str(link.attrs['href'])) and counter%2!=0 :
+                ('La-Liga' in str(link.attrs['href'])) and counter % 2 != 0:
             list_links.append('https://fbref.com'+str(link.attrs['href']))
+        else:
+            list_links.append("counter "+str(counter)+" not found")
+
     return list_links
 
 
 
 
 if __name__ == '__main__':
-    #print(create_url_season(2000))
-    print(get_link_report_match(create_url_season(2000)['2000-2001']))
+    print(create_url_season(2000))
+    # print(get_link_report_match(create_url_season(2000)['2000-2001']))
