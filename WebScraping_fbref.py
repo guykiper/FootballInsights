8a1f640a98ab8c242721251e4f523955c71dd1bb
guykiper <guykiper1@gmail.com>
2022-09-10 20:39:35 +0300

    creating 'get_link_report_match' function

--- WebScraping_fbref.py
+++ WebScraping_fbref.py
@@ -16,6 +16,26 @@ def create_url_season(year):
     return (dict_season_url)
 
 
+def get_link_report_match(url):
+    '''
+    :param url: url for the season
+    :return: list with link to each game in the season
+    '''
+    counter = 0
+    list_links =[]
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+    links = soup.find_all('a')
+    for link in links:
+        counter += 1
+        if ('href' in link.attrs) and ('matches' in str(link.attrs['href'])) and \
+                ('La-Liga' in str(link.attrs['href'])) and counter%2!=0 :
+            list_links.append('https://fbref.com'+str(link.attrs['href']))
+    return list_links
+
+
+
 
 if __name__ == '__main__':
-    print(create_url_season(2000))
+    #print(create_url_season(2000))
+    print(get_link_report_match(create_url_season(2000)['2000-2001']))
