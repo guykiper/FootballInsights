c9b02a849e63aa59cd8272aa08c3dd9ddcbf2612
guykiper <guykiper1@gmail.com>
2022-09-11 09:00:49 +0300

    creating the function "compation_df"

--- WebScraping_fbref.py
+++ WebScraping_fbref.py
@@ -2,7 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import regex
-
+import lxml
 
 def create_url_season(year):
     """
@@ -34,14 +34,26 @@ def get_link_report_match(url):
         if ('href' in link.attrs) and ('matches' in str(link.attrs['href'])) and \
                 ('La-Liga' in str(link.attrs['href'])) and counter % 2 != 0:
             list_links.append('https://fbref.com'+str(link.attrs['href']))
-        else:
-            list_links.append("counter "+str(counter)+" not found")
-
     return list_links
 
+def compation_df(url,comp ='La Liga') :
 
+    """
+    :param url: get url for a specific season ,example:create_url_all_seasons()['2019-2020']
+    :param comp:the compation of real madrid : La Liga,Champions Lg,Copa del Rey,Supercopa de Espana
+    :return: return the data frame of the comp that were chosen from the url
+    """
+    dfs = pd.read_html(url)
+    season_df = dfs[1]
+    #season_df.drop('Notes', inplace=True, axis=1)# remove column 'Notes'
+    season_df = season_df[season_df['Comp'] == comp]
+    season_df['Match Report'] = get_link_report_match(url) # edit the column 'Match Report' with the link of the match report
+    return season_df
 
 
 if __name__ == '__main__':
-    print(create_url_season(2000))
+    # print(create_url_season(2000))
     # print(get_link_report_match(create_url_season(2000)['2000-2001']))
+    url_season_2019 = create_url_season(2019)['2019-2020']
+    season_2019_2020 = compation_df(url_season_2019)
+    print(season_2019_2020)
