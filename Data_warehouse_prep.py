ba013c8070c8d20c30c2dcf052bb1a5905968238
guykiper <guykiper1@gmail.com>
2024-01-10 19:55:34 +0200

    add documentation to the code

--- Data_warehouse_prep.py
+++ Data_warehouse_prep.py
@@ -1,9 +1,30 @@
 import pandas as pd
 
+"""
+This create_tables class handles reading in the raw data CSV files 
+and transforming them into analytical tables for the football dataset.
+
+Key capabilities:
+
+- Joins the player data with the club data to add club codes
+- Subsets columns to create focused analytical tables  
+   (performance, playing time etc.)
+- Deduplicates nation location data
+- Provides helper functions like get_club_code to assist 
+  with data transformations
+
+The goal is to take the raw CSV files and output cleaned, 
+structured data tables for modeling and analysis.
+
+"""
 
 class create_tables:
 
     def __init__(self):
+        """
+        Constructor to load all the raw CSV files needed for processing.
+        """
+
         self.file_locatoin_info_male = pd.read_csv('Data_files/csv files/locaiton_info.csv')
         self.file_location_info_female = pd.read_csv('Data_files/csv files/locaiton_info_female.csv')
         self.file_players = pd.read_csv('Data_files/csv files/players.csv')
@@ -14,6 +35,10 @@ class create_tables:
 
 
     def get_club_code(self):
+        """
+        Joins club codes to the player table
+        and handles deduplication.
+        """
 
         merged = self.file_players.merge(self.club_and_code, how='left', left_on='team', right_on='club_name')
         # Drop 'club_name' column now
@@ -24,12 +49,14 @@ class create_tables:
 
 
     def create_teams_table(self):
+        """Cleans the Teams table"""
         teams_table = self.file_teams[['club_code','club_name','gender']]
         teams_table['country'] = [country.split()[0] for country in self.file_teams['country'] ]
         return teams_table
 
 
     def create_nation_table(self):
+        """Deduplicates nation info"""
         nation_table = pd.concat([self.file_locatoin_info_male, self.file_location_info_female])
         nation_table = nation_table.drop_duplicates(subset=['country_code'])
         nation_table = nation_table[['country_code', 'country_name', 'latitude', 'longitude']]
@@ -37,6 +64,7 @@ class create_tables:
 
 
     def create_players_performance_table(self):
+        """Creates a focused performance table"""
         perf_cols = []
         perf_cols.append('ID')
         perf_cols.append('season')
@@ -46,6 +74,7 @@ class create_tables:
         print('hi')
 
     def create_per_90_minutes_table(self):
+        """Creates a focused Per 90 stats table"""
         perf_cols = []
         perf_cols.append('ID')
         perf_cols.append('season')
@@ -55,6 +84,7 @@ class create_tables:
         print('hi')
 
     def create_player_expected_table(self):
+        """Creates a focused expected stats table"""
         perf_cols = []
         perf_cols.append('ID')
         perf_cols.append('season')
@@ -65,6 +95,7 @@ class create_tables:
 
 
     def create_player_playing_time_table(self):
+        """Creates a focused playing time stats table"""
         perf_cols = []
         perf_cols.append('ID')
         perf_cols.append('season')
@@ -74,6 +105,7 @@ class create_tables:
         print('hi')
 
     def create_player_progression_table(self):
+        """Creates a focused progression stats table"""
         perf_cols = []
         perf_cols.append('ID')
         perf_cols.append('season')
